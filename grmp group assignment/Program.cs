055a029186f190d3d11a40b2e499c0d667643a0c
visualstudio <visualstudio@122-PC12>
2019-02-22 18:05:39 -0500

    gagan

--- grmp group assignment/Program.cs
+++ grmp group assignment/Program.cs
@@ -9,7 +9,20 @@ namespace grmp_group_assignment
     class Program
     {
         static void Main(string[] args)
+        { }
+        class assignment1
         {
+            public assignment1(string note, int dist)
+            {
+                villageName = note; distanceTraveled = dist;
+                HowFarToGetBack = distanceTraveled;
+            }
+            public int HowFarToGetBack = 0;
+            private string villageName;
+            private int distanceTraveled;
+            public int getDistanceWalked() { return distanceTraveled; }
+            public string getVillageName() { return villageName; }
         }
     }
-}
+    }
+
