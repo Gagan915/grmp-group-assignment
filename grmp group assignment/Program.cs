3c5cf084881009b85a00bcce98e5b44be7c007b8
visualstudio <visualstudio@122-PC12>
2019-02-22 18:15:53 -0500

    6.15pm

--- grmp group assignment/Program.cs
+++ grmp group assignment/Program.cs
@@ -10,19 +10,19 @@ namespace grmp_group_assignment
     {
         static void Main(string[] args)
         { }
-        class assignment1
-        {
-            public assignment1(string note, int dist)
-            {
-                villageName = note; distanceTraveled = dist;
-                HowFarToGetBack = distanceTraveled;
-            }
-            public int HowFarToGetBack = 0;
-            private string villageName;
-            private int distanceTraveled;
-            public int getDistanceWalked() { return distanceTraveled; }
-            public string getVillageName() { return villageName; }
-        }
-    }
-    }
 
+    }
+}
+class assignment
+{
+    public assignment(string note, int dist)
+    {
+        villageName = note; distanceTraveled = dist;
+        HowFarToGetBack = distanceTraveled;
+    }
+    public int HowFarToGetBack = 0;
+    private string villageName;
+    private int distanceTraveled;
+    public int getDistanceWalked() { return distanceTraveled; }
+    public string getVillageName() { return villageName; }
+}
